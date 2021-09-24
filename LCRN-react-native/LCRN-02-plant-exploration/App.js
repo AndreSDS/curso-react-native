5a8cb9c23c14a39787c6bcf1b0472b2dcf1c394c
AndreSDS <andre_smiths@outlook.com>
2021-09-24 16:07:57 -0300

    iniciando projeto e configurando primeira tela

---
+++ LCRN-react-native/LCRN-02-plant-exploration/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Home} from './src/screens/index';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{headerShown: false}}
+        initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default () => <App />;
---
+++ LCRN-react-native/LCRN-02-plant-exploration/react-native.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  presets: {
+    ios: {},
+    android: {},
+  },
+  assets: ['./src/assets/fonts/'],
+};
